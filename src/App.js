dd97b192c8d740f4a74632a1a1f1284e2ec4d782
BTH-M3ND3S <mendessilveriomateus@gmail.com>
2023-08-16 14:58:22 -0300

    modelo react prompt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+import ExibeNota from "./components/Exibenota"
+import Contador from "./components/contador";
+import BoasVindas from "./components/BoasVindas";
 
-function App() {
+
+function App(props) {
+
+  
+
+
+  let nota = props.nota;
+  let aprovado = false;
+  let reprovado = false;
+  if(nota > 5){
+    aprovado = true;
+  } else {
+    reprovado= true;
+  }
+
+  
+
   return (
     <>
+      { aprovado && 
+      <ExibeNota mensagem="aprovado" nota={props.nota}/>
+       }
+      { reprovado && 
+      <ExibeNota mensagem="reprovado" nota={props.nota}/>
+      }  
+      <Contador/>
+      <BoasVindas/>
     </>
   );
 }
